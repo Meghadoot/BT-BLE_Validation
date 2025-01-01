6d8f70dc3dcb35243552e7a4761574d1ad555fda
Meghadoot Gardi <rkomeghadoot@gmail.com>
2025-01-01 22:36:27 +0100

    Add Initial Config Header

---
+++ Communication_Tests/BT-BLE_Test_Config.c
@@ -0,0 +1,48 @@
+/***************************************************************************
+	BT-BL-Test_Config.h
+	Author: Meghadoot
+  	2025-01-02
+	
+	This file is a part of library for the validation of Bluetooth Interface.
+	This file holds a global configs and macros for tests associated with Communication tests.
+	
+    Copyright (C) 2024  Meghadoot Gardi.  All right reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ ***************************************************************************/
+
+/** TODO
+* Create headers 
+
+**/
+
+
+
+
+#ifndef _VV_BT_BLE_Config_h
+#define _VV_BT_BLE_Config_h
+
+#include <stdio.h>
+#include <stdlib.h>
+/*=========================================================================
+    MACRO DECLARATIONS
+    -----------------------------------------------------------------------*/
+    #define Target_BT_BLE_IPV6        XX:XX:XX:XX:XX:XX
+ 
+    #define __VV_BT_BLE_debug         DISABLED
+/*=========================================================================*/
+
+
+#endif
